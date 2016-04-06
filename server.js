d8f8ab30a2600d540274b4a36fd24d1746ea531c
guannanxia <guannanxia@meilishuo.com>
2016-04-06 14:56:47 +0800

    追加结果表示页面

--- server.js
+++ server.js
@@ -14,21 +14,33 @@ app.use('/static', express.static('./static'));
 app.get('/', function(req, res) {
     res.render('photo');
 });
+app.get('/result', function(req, res) {
+  var InvitationModel = mongoose.model('invitation');
+//  var invitation = new InvitationModel();
+  console.log(InvitationModel.find);
+
+  InvitationModel.find({}).select('username phone count comment').exec(function(err, result){
+      if (err) {
+        console.log('err', err);
+      }
+      res.render('result', {list:result});
+  });
+});
 app.post('/', function(req, res) {
     var InvitationModel = mongoose.model('invitation');
     var invitation = new InvitationModel();  
-    console.log('usrename', req.body.name);
+//    console.log('usrename', req.body.name);
     invitation.set('username',req.body.name);
     invitation.set('phone',req.body.phone); 
     invitation.set('count',req.body.count);
     invitation.set('comment',req.body.comment);
     invitation.save(function(err) {
         if (err) {
-	    console.log('err', err);
-	}
-        res.write(JSON.stringify(invitation.toJSON()));
-        res.end();
+          console.log('err', err);
+        }
     });
+    res.write(JSON.stringify(invitation.toJSON()));
+//    res.redirect("www.baidu.com");
 
-})
+});
 app.listen(8081);
